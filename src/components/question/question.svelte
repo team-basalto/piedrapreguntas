bd8ef1135cb50e5b03ba0066a13dd55cb40e8b48
Jaime Torres <156240086+mews6@users.noreply.github.com>
2024-11-23 16:01:35 -0500

    add component logic

---
+++ src/components/question/question.svelte
@@ -0,0 +1,22 @@
+<script>
+    import 'bootstrap-grid.css'
+    import Button from '../../stories/Button.svelte';
+    const {question_image,question_text,correct_answer} = $props()
+</script>
+
+<div class="container-fluid">
+    <div class = "card">
+        <img src= '${ question_image }' class="card-img-top" alt="...">
+        <div class="card-body">
+            <h2> 
+                ${ question_text }
+            </h2>
+            <div class = "row">
+
+            </div>
+            <div class = "row">
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/stories/question.stories.svelte
@@ -0,0 +1,15 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import Question  from '../components/question/question.svelte';
+    import { fn } from '@storybook/test';
+
+    const { Story } = defineMeta({
+    title: 'Example/Question',
+    component: Question,
+    tags: ['autodocs'],
+    argTypes: {
+    },
+    args: {
+    }
+  });
+</script>
