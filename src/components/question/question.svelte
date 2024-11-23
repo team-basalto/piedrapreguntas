b3cde5d3c376231f947ed86899cfc403e978d903
Jaime Torres <156240086+mews6@users.noreply.github.com>
2024-11-23 16:12:08 -0500

    add component for question

--- src/components/question/question.svelte
+++ src/components/question/question.svelte
@@ -1,7 +1,12 @@
 <script>
     import 'bootstrap-grid.css'
     import Button from '../../stories/Button.svelte';
-    const {question_image,question_text,correct_answer} = $props()
+    const {question_image,question_text,correct_answer,
+        option_a,
+        option_b,
+        option_c,
+        option_d
+    } = $props()
 </script>
 
 <div class="container-fluid">
@@ -12,10 +17,12 @@
                 ${ question_text }
             </h2>
             <div class = "row">
-
+                <Button size='medium' label = '${ option_a }'> </Button>
+                <Button size='medium' label = '${ option_b }'> </Button>
             </div>
             <div class = "row">
-
+                <Button size='medium' label = '${ option_c }'> </Button>
+                <Button size='medium' label = '${ option_d }'> </Button>
             </div>
         </div>
     </div>
